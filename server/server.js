1f8d20c505b7c91eb2524aa75c5b5c7a3e1847c6
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-08-16 18:09:46 +0400

    update

--- server/server.js
+++ server/server.js
@@ -6,12 +6,3 @@ const mysql = require('mysql2')
 const server = app.listen(process.env.PORT, () => {
     console.log('server has started')
 })
-
-const db = mysql.createConnection({
-    host: "sql110.ezyro.com",
-    user: "ezyro_37100528",
-    password: "a04e56b9ac1",
-    database: "ezyro_37100528_songs",
-    port: "3306"
-}).promise()
-
