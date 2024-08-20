5c11d03b0a35711858d6e081f8166ebcbf34fdd8
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-08-20 18:46:30 +0400

    deletin mysql package

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 const app = require('./app')
-const mysql = require('mysql2')
 
 const server = app.listen(process.env.PORT, () => {
     console.log('server has started')
